4600c32041271892a6a0feada3cf05a5e7a36507
Twicster <mathias.duedahl@gmail.com>
2022-02-23 15:54:00 -0800

    Testing code

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # Stich together halftoning and graphsolving here and plot it
-from curses.ascii import isalnum
+#from curses.ascii import isalnum
+from sympy import ordered
 from graph import Graph
 import halftoning
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Just for testing atm:
-X = np.random.randint(0,10,10)
-Y = np.random.randint(0,10,10)
+X = np.random.randint(0,100,100)
+Y = np.random.randint(0,100,100)
 nodes = list(zip(X,Y))
 
-#G = Graph(nodes)
-#print(G.distM)
-print(isalnum(2,float))
+G = Graph(nodes)
+fig, ax = plt.subplots(1,1)
+ax.plot(X,Y,"b.")
+
+tspsol = G.TSP()
+print(tspsol)
+
+ax.plot(tspsol[:,0],tspsol[:,1])
+plt.show()
