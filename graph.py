26171d905d55d68ab99f4e09c9c243dffc3d9ee5
Twicster <mathias.duedahl@gmail.com>
2022-02-23 15:53:42 -0800

    TSP solver working. Returns ordered nodes.

--- graph.py
+++ graph.py
@@ -22,11 +22,13 @@ class Graph:
         if not isinstance(nodes,list):
             raise TypeError("Nodes must be a list")
         if not all([isinstance(i,tuple) and len(i)==2 for i in nodes]):
-            raise TypeError("All node entries must be 2-tuples")    
-        if not all([isinstance(i[0],(int,float)) and isinstance(i[1],(int, float)) for i in nodes])
+            raise TypeError("All node entries must be 2-tuples")
+        typewhitelist = (int, float,np.int32,np.float64)
+        if not all([isinstance(i[0],typewhitelist) and isinstance(i[1],typewhitelist) for i in nodes]):
             raise TypeError("All coordinates must be int / floats")
         # FIXME: Add error handling, all coordinates must be int / floats
         
+        self.nodes = nodes
         self.distM = self.createDistMatrix(nodes)
 
     def createDistMatrix(self, nodes):
@@ -47,8 +49,54 @@ class Graph:
                 d = np.sqrt((P1[0]-P2[0])**2+(P1[1]-P2[1])**2)
                 dist[i][j], dist[j][i] = d, d
         return dist
-    
+
+
     def TSP(self, scalingfactor = 100):
         #Solve TSP here and return graph object with adjacencylist containing the edges
         #approximated Hamiltonian cycle.
-        pass
+        #From OR tools documentation:
+
+        # Structure needed for package TSP solver.
+        # depot -> starting node
+        # num_vehicles -> number of "salesmen"
+        data = {"distance_matrix":self.distM,"num_vehicles":1,"depot":0}
+        manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),data['num_vehicles'], data['depot'])
+        routing = pywrapcp.RoutingModel(manager)
+
+        def distance_callback(from_index, to_index):
+            """Returns the scaled integer distance between the two nodes.
+
+            Args:
+                from_index (int): index of start node
+                to_index (int): index of end node
+                scalingfactor (float): factor with which to scale distances
+                manager (RoutingIndexManager): Manager object from pywrapcp
+            """
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data['distance_matrix'][from_node][to_node]*scalingfactor
+
+        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+        #Set cost function
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+        # Set search parameters and heuristic
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        
+        # Solve
+        solution = routing.SolveWithParameters(search_parameters)
+
+        # Get the approximated optimal order of nodes for the shortest path as a list of indexes
+        index = routing.Start(0)
+        route = [manager.IndexToNode(index)]
+        while not routing.IsEnd(index):
+            index = solution.Value(routing.NextVar(index))
+            route.append(manager.IndexToNode(index))
+
+        # Return reordered array of nodes.
+        nodarray = np.array(self.nodes)
+        return nodarray[route]
+    
