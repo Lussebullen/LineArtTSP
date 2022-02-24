ef35a06380318f36d51f4416e2bc8eaa03f48af6
Twicster <mathias.duedahl@gmail.com>
2022-02-23 17:27:03 -0800

    Restructuring and testing

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 # Stich together halftoning and graphsolving here and plot it
 #from curses.ascii import isalnum
-from sympy import ordered
+#from sympy import ordered
 from graph import Graph
 import halftoning
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Just for testing atm:
-X = np.random.randint(0,100,100)
-Y = np.random.randint(0,100,100)
+# Generate test data points
+n=50
+size = 1000
+X = np.random.randint(0,size,n)
+Y = np.random.randint(0,size,n)
 nodes = list(zip(X,Y))
 
+# Create graph object solve TSP
 G = Graph(nodes)
+tspsol = G.TSP(timelimit=20)
+
+# Plot points and approximate optimal path
 fig, ax = plt.subplots(1,1)
-ax.plot(X,Y,"b.")
-
-tspsol = G.TSP()
-print(tspsol)
-
+ax.plot(X,Y,"b,")
 ax.plot(tspsol[:,0],tspsol[:,1])
 plt.show()
+
+print()
