1e040db64b1774186eaedd283100d418695bb4f3
aneveu23 <aneveu2323@g.ucla.edu>
2022-03-10 14:16:30 -0800

    renaming variables

--- halftoning.py
+++ halftoning.py
@@ -16,9 +16,10 @@ def rejectionSampling(n, M, imagestyle="brightness"):
     """
     #CONTRAST METHOD 
     if imagestyle == "contrast":
+        pixel_distance = 5
         img_pixels = np.array(list(M.getdata())).reshape((M.size[1], M.size[0], 3))/255
         #play around with the i-5 and range(5, ..); change 5 to various values
-        contrast_array = np.array([[np.linalg.norm(img_pixels[i, j, :] - img_pixels[i-5, j, :])/np.sqrt(3) for j in range(M.size[0])] for i in range(5, M.size[1])])
+        contrast_array = np.array([[np.linalg.norm(img_pixels[i, j, :] - img_pixels[i-pixel_distance, j, :])/np.sqrt(3) for j in range(M.size[0])] for i in range(5, M.size[1])])
         #I set up the following code identically
         r = len(contrast_array)
         c = len(contrast_array[0]) 
@@ -29,8 +30,8 @@ def rejectionSampling(n, M, imagestyle="brightness"):
             x = int(rd.uniform(0, c))
             y = int(rd.uniform(0, r))
             #play around with this u constant; it determines what level of contrast is considered.
-            u = 0.15
-            if contrast_array[y][x] > u:
+            contrast_threshold = 0.15
+            if contrast_array[y][x] > contrast_threshold:
                 X[a] = x
                 Y[a] = y
                 a += 1
