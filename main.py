39dde162379abff8b18d9a7916e16cf5bf3108f1
Twicster <mathias.duedahl@gmail.com>
2022-03-10 20:11:52 -0800

    Adjusted main code to fit new halftoning

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-# Stich together halftoning and graphsolving here and plot it
-#from curses.ascii import isalnum
-#from sympy import ordered
 from graph import Graph
 from halftoning import rejectionSampling
-import numpy as np
-from PIL import Image
 
 # Generate test data points
-n=1000
-# Load Image, resize and convert to grayscale matrix with elements in [0,1], 0=black, 1=white.
-img = Image.open('C:\\Users\\Antoine Neveu\\Desktop\\TSPProject\\LineArtTSP\\SFJxCXKMZihnZsVnn3LoEk-1200-80.jpg')   # FIXME: remove later, load image in main.
-img = img.rotate(180)
-###I(Toine) moved conversion to grayscale, array creation into halftoning in order to allow me to add contrast method 
-nodes = rejectionSampling(n, img, imagestyle = "contrast")
-
-#X = np.random.randint(0,size,n)
-#Y = np.random.randint(0,size,n)
-#nodes = np.array(list(zip(X,Y)))
+n=200
+# Path to image file
+path = 'recurrentTheme.jpg'
+# Generate points
+nodes = rejectionSampling(n, path, imagestyle = "brightness")
 
 # Create graph object solve TSP
 G = Graph()
 G.setNodes(nodes)
 G.setDistMatrix()
-G.TSP(timelimit=40)
+G.TSP(timelimit=30)
 
 # Plot points and approximate optimal path
 G.plot(style="point")
