e6fc6ff275ccc95a856032ecb35e6ec0d876ce90
aneveu23 <aneveu23@g.ucla.edu>
2022-03-13 17:48:01 -0700

    Added pixel_shift per dimension, updated docstring

--- halftoning.py
+++ halftoning.py
@@ -1,17 +1,22 @@
 import random as rd
 import numpy as np
 from PIL import Image, ImageOps
+import math
 
 ########################################################################################################################
 # Image stippling, take the image as input and do all the processing to generate stippling vertices.
 ########################################################################################################################
 # Used this source for Stippling: https://www.cs.ubc.ca/labs/imager/tr/2002/secord2002b/secord.2002b.pdf
 
-def rejectionSampling(n, path, imagestyle="brightness", pixel_distance = 5, contrast_threshold = 0.15, smoothing_constant = 0):
+def rejectionSampling(n, path, imagestyle="brightness", x_pixel_distance = 5, y_pixel_distance = 5, contrast_threshold = 0.15, smoothing_constant = 0):
     """
     :param n:   Desired amount of samples
     :param path:   path to image file for halftoning
     :return:    Set of crude initial stippling points
+    :imagestyle: brightness or contrast as a method of point selection
+    :x_pixel_distance: and :y_pixel_distance: the pixel shifts used to determine contrast between pixels
+    :contrast_threshold: the value about which a threshold for point selection is formed - 100% acceptance if smoothing_constant = 0 and contrast exceeds threshold
+    :smoothing_constant: Linear smoothening of probability of a point being selected about the contrast threshold - allows for lower contrast to be accepted, and higher contrast to be accepted less frequently
     """
     # Rotate and flip image
     M = Image.open(path).rotate(180)
@@ -19,12 +24,19 @@ def rejectionSampling(n, path, imagestyle="brightness", pixel_distance = 5, cont
 
     #CONTRAST METHOD 
     if imagestyle == "contrast":
+        #creating NP array based on image
         M_array = np.array(list(M.getdata()))
+        #Checking if image has RGB pixels - if not, 'broadcasts' to RGB
         if M_array.size != M.size[1]*M.size[0]*3:
             M_array = np.stack((M_array,)*3, axis = 1)
+        #reshaping array into image dimensions
         img_pixels = M_array.reshape((M.size[1], M.size[0], 3))/255
-        #play around with the i-5 and range(5, ..); change 5 to various values
-        contrast_array = np.array([[np.linalg.norm(img_pixels[i, j, :] - img_pixels[i-pixel_distance, j-pixel_distance, :])/np.sqrt(3) for j in range(pixel_distance, M.size[0])] for i in range(pixel_distance, M.size[1])])
+        #creating shifted contrast array 
+        if abs(x_pixel_distance) > M.size[0]:
+            raise ValueError("x pixel shift exceeds number of x pixels")
+        if abs(y_pixel_distance) > M.size[1]:
+            raise ValueError("y pixel shift exceeds number of y pixels")
+        contrast_array = np.array([[np.linalg.norm(img_pixels[i, j, :] - img_pixels[i-x_pixel_distance, j-y_pixel_distance, :])/np.sqrt(3) for j in range(max(200,y_pixel_distance), min(M.size[0], M.size[0]+y_pixel_distance))] for i in range(max(0,x_pixel_distance), min(M.size[1], M.size[1]+x_pixel_distance))])
         #I set up the following code identically
         r = len(contrast_array)
         c = len(contrast_array[0]) 
